d90f43e1a1cbe99e494977d66afe856e18d3a6e6
progress0407 <progress0407@gmail.com>
2023-08-29 23:50:57 +0900

    BaseBallGame 전반적인 리펙터링

--- baseball/src/main/kotlin/app/BaseBallGame.kt
+++ baseball/src/main/kotlin/app/BaseBallGame.kt
@@ -4,26 +4,26 @@ import app.io.IoWrapper
 import app.participant.Computer
 import app.participant.Player
 import java.util.Random
-import java.util.Scanner
 import kotlin.random.asKotlinRandom
 
 class BaseBallGame(private val io: IoWrapper) {
 
     fun run() {
         while (true) {
-            val computer = Computer(computerNumbers())
+            val computer = Computer(generateComputerBallNumbers())
             println("### Computer Number = ${computer.ballNumbers} ###")
+
             while (true) {
                 println("숫자를 입력해 주세요 : ")
-                val numbersString = io.inputString()
-                val numbers = convertToValueObjects(numbersString)
+                val numbers = inputPlayerBallNumbers()
                 val player = Player(numbers)
                 val matchResults = computer.match(player)
-                println(view(matchResults))
+                matchResults.view()
                 if (matchResults.isAllStrike()) {
                     break
                 }
             }
+
             println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
             val continueJudgeNumber = io.inputNumber()
             if (continueJudgeNumber == 2) {
@@ -32,21 +32,24 @@ class BaseBallGame(private val io: IoWrapper) {
         }
     }
 
-    private fun computerNumbers(): List<BallNumber> {
-
+    private fun generateComputerBallNumbers(): List<BallNumber> {
         val n = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
         val r = 3
         val ballNumbers = permutation(n, r)[randomNumber()]
             .mapIndexed { index, value -> BallNumber(index, value) }
-
         return ballNumbers
     }
 
+    private fun inputPlayerBallNumbers(): List<BallNumber> {
+        val numbersString = io.inputString()
+        val numbers = convertToValueObjects(numbersString)
+        return numbers
+    }
 
-
-    private fun convertToValueObjects(numbersString: String) = numbersString.toCharArray().withIndex()
-        .map { BallNumber(it.index, it.value.code - '0'.code) }
-        .toList()
+    private fun convertToValueObjects(numbersString: String) =
+        numbersString.toCharArray().withIndex()
+            .map { BallNumber(it.index, it.value.code - '0'.code) }
+            .toList()
 
     private fun randomNumber(): Int {
         return Random().asKotlinRandom().nextInt(0, 9 * 8 * 7 + 1)
@@ -54,16 +57,13 @@ class BaseBallGame(private val io: IoWrapper) {
 
     private fun view(result: MatchResults) =
         when {
-            result.ballAndStrikeCase() ->
-                "${result.ball}볼 ${result.strike}스트라이크"
-
-            result.ballCase() ->
-                "${result.ball}볼"
-
-            result.strikeCase() ->
-                "${result.strike}스트라이크"
-
-            else ->
-                "낫싱"
+            result.ballCase() -> "${result.ball}볼"
+            result.strikeCase() -> "${result.strike}스트라이크"
+            result.ballAndStrikeCase() -> "${result.ball}볼 ${result.strike}스트라이크"
+            else -> "낫싱"
         }
+
+    private fun MatchResults.view() {
+        println(view(this))
+    }
 }
